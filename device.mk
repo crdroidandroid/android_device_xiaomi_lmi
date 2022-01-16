8c6591d0abc016a606300d77cf68d5e7bed07506
RealSchnedi <schnedi3@gmail.com>
2022-01-16 16:13:45 +0100

    lmi: Revert and cleanup
    * Revert "lmi: Opt-in for FOD"

--- device.mk
+++ device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Fingerprint
-TARGET_HAS_FOD := true
-
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
@@ -17,14 +14,6 @@ $(call inherit-product, vendor/xiaomi/lmi/lmi-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 29
 
