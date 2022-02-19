30c920a7e8c1aa887309b9f186379ab09b01f447
TheScarastic <warabhishek@gmail.com>
2022-02-19 17:59:47 -0300

    lmi: parts: Adapt UI to Android 12 style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to preference screen
    
    [mickaelmendes50]
     * Adapt to Popup Settings
    
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Change-Id: I331fb590464f423d321868dc6b5930146eb1f10a

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsFragment.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsFragment.java
@@ -19,17 +19,15 @@ package org.lineageos.devicesettings.popupcamera;
 import android.os.Bundle;
 import android.view.MenuItem;
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 import org.lineageos.devicesettings.R;
 
 public class PopupCameraSettingsFragment
-    extends PreferenceFragment implements OnPreferenceChangeListener {
+    extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     addPreferencesFromResource(R.xml.popup_settings);
-    getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
   }
 
   @Override
