87b29b1fb84ce50bc10607911938e65e3086ff6e
Etienne Ruffieux <eruffieux@google.com>
2022-08-16 21:52:50 +0700

    lmi: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba

--- device.mk
+++ device.mk
@@ -109,7 +109,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
