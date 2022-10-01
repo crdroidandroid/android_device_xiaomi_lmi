afc71333495519d8fa7544720786af1a4800a8da
¨raystef66¨ <s.vanbarel@gmail.com>
2022-10-01 15:31:28 +0200

    Revert "lmi: Enable Project ID Quota support on userdata."
    This reverts commit 8cda9f215c5e81d6c945418c9ecde5abf7a59a4a.

--- device.mk
+++ device.mk
@@ -10,9 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Project ID Quota
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
