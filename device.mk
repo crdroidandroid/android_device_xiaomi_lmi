b6b552084af056c75718c0614cf76eb50a3215aa
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-20 05:01:38 +0530

    lmi: Build IFAAService from source
    Change-Id: I804f0a424010cefaa3cc6f394c716b8b43302466

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
