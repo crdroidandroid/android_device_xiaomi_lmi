f3220fd53ebdb0c7a0939532e53a549c6fb71c50
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-31 11:19:04 +0200

    lmi: Switch to QTI Memtrack AIDL HAL

--- device.mk
+++ device.mk
@@ -187,15 +187,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1 \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.kona \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    memtrack.kona \
     vendor.display.config@1.0 \
     vendor.display.config@1.9 \
     vendor.display.config@1.9.vendor \
@@ -207,7 +204,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # DRM
 PRODUCT_PACKAGES += \
