02ea8c31023810804d6b72f1cb8a70307edc3a3a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-03 03:21:44 +0200

    lmi: media: Use media_codecs_kona_vendor.xml
    Despite Xiaomi made changes to the non-vendor variant, this is what a
    QSSI would load...
    
    Change-Id: Id10feea9e6c44177213696eb0d0165af1abf3ac9

--- device.mk
+++ device.mk
@@ -293,9 +293,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     media_codecs_kona.xml \
-    media_codecs_kona_vendor.xml \
-    media_codecs_performance_kona.xml \
-    media_codecs_system_default.xml
+    media_codecs_performance_kona.xml
 
 # NFC
 PRODUCT_PACKAGES += \
