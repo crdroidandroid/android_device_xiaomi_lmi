abb8f45b2d4ed421f0d75b157ed45dfba0ac85cf
basamaryan <basam.aryan@gmail.com>
2024-09-29 16:24:14 -0400

    lmi: Move to Xiaomi fingerprint AIDL
    Change-Id: I68dba7184034fd37454c7d32a065f041cb611625

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_COPY_FILES += \
 
 # Biometrics
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.xiaomi
 
 PRODUCT_PACKAGES += \
     libudfpshandler
