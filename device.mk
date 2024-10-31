a804a6ae59a0074332903ce61c316db972507a22
LuK1337 <priv.luk@gmail.com>
2024-10-31 11:39:57 +0100

    lmi: Build libprotobuf-cpp-lite-3.9.1-vendorcompat * CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.3-service.widevine": library "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/libwvhidl.so in namespace

--- device.mk
+++ device.mk
@@ -386,7 +386,8 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QMI
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
