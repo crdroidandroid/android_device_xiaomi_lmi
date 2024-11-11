b7116c7ed88466243092b97ee9f8aa4a9ffcccc6
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-11 22:07:39 +0100

    lmi: Import QTI Codec2 service and configs
    From munch
    
    Change-Id: I17a56c596f15a1d5c23582c5c3a7028d3e676aab

--- device.mk
+++ device.mk
@@ -288,6 +288,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
 PRODUCT_PACKAGES += \
+    media_codecs_c2.xml \
+    media_codecs_performance_c2.xml \
     media_codecs_kona.xml \
     media_codecs_performance_kona.xml
 
