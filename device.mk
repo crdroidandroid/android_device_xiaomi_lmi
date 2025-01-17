ab3eb4c74ab17b8356329f60436f9edef63052f7
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    lmi: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -342,7 +342,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/lmi-miuicamera/config.mk)
