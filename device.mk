0b5d22edec73149955f6be239eeb49caba380be5
¨raystef66¨ <s.vanbarel@gmail.com>
2025-01-18 12:52:41 +0100

    lmi: remove offending entry

--- device.mk
+++ device.mk
@@ -179,7 +179,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.kona \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
