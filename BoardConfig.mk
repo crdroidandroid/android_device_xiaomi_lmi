2684511422596784733aa147ad079b342a761a6c
onettboots <blackcocopet@gmail.com>
2025-01-18 12:53:22 +0100

    lmi: kernel: Update flags for clang 20+ clang: error: version 'kernel' in target triple 'arm-unknown-linux-androidkernel' is invalid

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,10 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1 \
     LLVM_IAS=1
 
+# Kernel Clang Flags
+KERNEL_CC := CC=clang
+override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
+
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
