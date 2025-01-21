fe4c06b797a598abfcd2e7f022bdf364a0518dbf
LuK1337 <priv.luk@gmail.com>
2025-01-21 13:25:21 +0100

    lmi: Switch to py extract utils
    Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

---
+++ extract-files.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project  
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {  
+    'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),
+    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
+    'vendor/etc/seccomp_policy/codec2.vendor.base-arm.policy': blob_fixup()
+        .regex_replace('futex: 1', '')
+        .add_line_if_missing('futex: 1'),
+    'vendor/etc/seccomp_policy/codec2.vendor.ext-arm.policy': blob_fixup()
+        .regex_replace('_llseek: 1', '')
+        .regex_replace('getdents64: 1', ''),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib/hw/audio.primary.lmi.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so',
+        )
+        .binary_regex_replace(
+            b'/vendor/lib/liba2dpoffload.so',
+            b'liba2dpoffload_lmi.so\x00\x00\x00\x00\x00\x00\x00\x00',
+        ),
+    'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+    (
+        'vendor/lib64/libMIAIHDRhvx_interface.so',
+        'vendor/lib64/libarcsoft_hdrplus_hvx_stub.so',
+        'vendor/lib64/libarcsoft_super_night_raw.so',
+        'vendor/lib64/libmialgo_rfs.so',
+    ): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open')
+        .clear_symbol_version('remote_register_buf')
+        .clear_symbol_version('remote_register_buf_attr'),
+    (
+        'vendor/lib64/libalAILDC.so',
+        'vendor/lib64/libalLDC.so',
+        'vendor/lib64/libalhLDC.so',
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),  
+} # fmt: skip
+
+namespace_imports = [
+    'device/xiaomi/lmi',
+    'hardware/qcom-caf/sm8250',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display', 
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    'com.qualcomm.qti.dpm.api@1.0',
+    'libmmosal',
+    'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+}
+
+module = ExtractUtilsModule(
+    'lmi',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
