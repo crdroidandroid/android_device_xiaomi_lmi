8cf862098bc2fb7ca8df2e23450142c81a8de85e
johnmart19 <johnivan19999@gmail.com>
2025-04-27 15:06:00 +0300

    lmi: Guard Qualcomm thermal service hals * Crashing on Alioth and some other devices
    * Crash:
     * Earlier versions:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
     * Current one:
        - E ThermalManagerService$ThermalHalWrapper: Couldn't getCurrentCoolingDevices due to invalid status
        - E ThermalManagerService$ThermalHalWrapper: java.lang.IllegalArgumentException: ThermalHAL not initialized properly.

--- device.mk
+++ device.mk
@@ -471,10 +471,12 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
     android.hardware.thermal@2.0.vendor \
     android.hardware.thermal-service.qti
+endif
 
 # Touchscreen
 PRODUCT_PACKAGES += \
