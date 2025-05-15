5d3ba87a79d9d9c25ebfac5cfb2935969a92929d
Michael Bestas <mkbestas@lineageos.org>
2025-05-15 13:31:16 +0200

    lmi: Move Lineage Health HAL to select() - Its deprecated
    vendor/lineage/config/BoardConfigSoong.mk:66: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass

--- device.mk
+++ device.mk
@@ -261,6 +261,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
