c366031b9c2e51ea53d44c797f1df9c6918d63e1
Uday Kumar Chunduru <udaykumarchunduru54@gmail.com>
2025-07-09 15:27:00 +0530

    lmi: Implement ViPER4AndroidFX support

--- device.mk
+++ device.mk
@@ -511,6 +511,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/aidl/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# ViPER4AndroidFX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
