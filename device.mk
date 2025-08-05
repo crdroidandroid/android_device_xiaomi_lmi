5f1a719899c964fb9464fed82c2fefb63ebf73f3
John Macka <macka2004@gmail.com>
2025-08-05 14:04:29 +1000

    lmi: audio: import audio effects https://github.com/PixelOS-Devices/android_device_xiaomi_sm8250-common

--- device.mk
+++ device.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
